620731f9f9fca743aa02446e0ef277acae8c2ae8
GSpadolini <giancarlospadolini@gmail.com>
2026-02-25 16:33:42 +0100

    Hola Mundo

---
+++ alquiler-service/src/main/java/com/alquilatus/vehiculos/controller/HolaController.java
@@ -0,0 +1,11 @@
+package com.alquilatus.vehiculos.controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HolaController {
+    @GetMapping("/")
+    public String hola() {
+        return "¡Hola, mundo!";
+    }
+}
